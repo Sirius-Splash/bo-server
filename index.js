b98aaf9ac039ca40b8a52d7d1976ec9952fa4cd0
Jimmie <legendaryprophecy@gmail.com>
2023-07-29 11:14:56 -0400

    Complete basic add user and find all users in controllers

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const app = express()
 const PORT = 8080
+const controllers = require('./data/controllers')
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 
-app.use('/', (req, res)=>{
-  res.sendStatus(404)
-})
+// app.use('/', (req, res)=>{
+//   res.sendStatus(404)
+// })
+
+app.post('/user', controllers.addUser);
+// app.get('/users', controllers.getUsers);
 
 app.listen(PORT)
 console.log(`Listening on port ${PORT}`)
