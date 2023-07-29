363587c66a9442716811ef1cb8ee5d6bfb70a11a
Jimmie <legendaryprophecy@gmail.com>
2023-07-29 11:38:03 -0400

    Added 404 unfound catch

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const controllers = require('./data/controllers')
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 
-// app.use('/', (req, res)=>{
-//   res.sendStatus(404)
-// })
 
 app.post('/user', controllers.addUser);
 // app.get('/users', controllers.getUsers);
+
+app.use('/', (req, res)=>{
+  res.sendStatus(404)
+});
 
 app.listen(PORT)
 console.log(`Listening on port ${PORT}`)
