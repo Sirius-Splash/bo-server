c11ac29fd41b78a78db8c058db7de0a2ff3e3482
d-verhnjak <darko.verhnjak@gmail.com>
2023-08-02 11:59:04 -0400

    Added basic CORS

--- data/controllers/index.js
+++ data/controllers/index.js
@@ -2,13 +2,15 @@ const models = require('../models');
 const prisma = models.db;
 
 module.exports.addUser = async (req, res) => {
-  let userData = req.body;
-  try {
-    await prisma.user.create({data: userData});
-    res.sendStatus(201);
-  } catch {
-    res.sendStatus(500);
-  }
+  // let userData = req.body;
+  // try {
+  //   await prisma.user.create({data: userData});
+  //   res.sendStatus(201);
+  // } catch {
+  //   res.sendStatus(500);
+  // }
+  console.log("HEARD")
+  console.log(req);
 }
 
 module.exports.getUsers = async (req, res) => {
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors');
 const bodyParser = require('body-parser')
 const gpt = require('./data/controllers/gpt.js')
 const app = express()
 const PORT = 8080
 const controllers = require('./data/controllers')
 
+app.use(cors());
+app.use(express.json());
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use("/gpt", gpt());
