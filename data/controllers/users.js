aebb68a02b239f88df77f65b4a44b85b2a769bf6
d-verhnjak <darko.verhnjak@gmail.com>
2023-08-02 12:34:30 -0400

    Almost finished with sending to DB

--- data/controllers/users.js
+++ data/controllers/users.js
@@ -1,19 +1,20 @@
-const models = require('../models');
-const prisma = models.db;
+const prisma = require("../models/index.js").db;
 
 module.exports.addUser = async (req, res) => {
-  // let userData = req.body;
-  // try {
-  //   await prisma.user.create({data: userData});
-  //   res.sendStatus(201);
-  // } catch {
-  //   res.sendStatus(500);
-  // }
-  const user = req.body;
+  const userData = req.body;
   user.online_status = false;
   user.profile_pic = '';
   console.log(user);
 
+
+
+  try {
+    await prisma.user.create({data: userData});
+    res.sendStatus(201);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
 }
 
 module.exports.getUsers = async (req, res) => {
